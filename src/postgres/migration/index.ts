20313069cf902e32b6c9211fa1e1a7dc41d714ae
workingdanny <asds8902@gmail.com>
2023-12-24 23:56:15 +0900

    don't export from postgres/migrations yet

--- src/postgres/migration/index.ts
+++
@@ -1,9 +0,0 @@
-import * as pg from "pg";
-
-import { DB_URL } from "Hexai/config";
-
-export async function runMigration() {
-    const client = new pg.Client({
-        connectionString: DB_URL,
-    });
-}
