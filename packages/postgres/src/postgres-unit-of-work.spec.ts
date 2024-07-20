fa76945c94caa0b48f2e36cf70d872ad9821a128
Sangwoo Hyun <22994241+workingdanny911@users.noreply.github.com>
2024-07-20 07:03:13 +0000

    conform to the changes in core, rewrite uow tests

--- packages/postgres/src/postgres-unit-of-work.spec.ts
+++ packages/postgres/src/postgres-unit-of-work.spec.ts
@@ -1,40 +1,105 @@
 import { beforeAll, beforeEach, describe, expect, test } from "vitest";
-import * as pg from "pg";
-import _ from "lodash";
+import { Client } from "pg";
+import { Propagation, UnitOfWorkAbortedError } from "@hexai/core";
 
-import {
-    IsolationLevel,
-    Propagation,
-    UnitOfWorkAbortedError,
-} from "@hexai/core";
-import { DB_URL } from "./config";
 import { PostgresUnitOfWork } from "./postgres-unit-of-work";
 import { DatabaseManager, replaceDatabaseName, TableManager } from "./helpers";
+import { DB_URL } from "./config";
 
-const DATABASE = "test_hexai__uow";
-const TABLE = "_test";
+const DATABASE = "hexai__test_uow";
 const URL = replaceDatabaseName(DATABASE, DB_URL);
+
+interface Entry {
+    id: number;
+    value: string;
+}
+
+class EntryRepository {
+    private static TABLE_NAME = "entries";
+
+    constructor(private client: Client) {}
+
+    public static async setup(client: Client) {
+        await new TableManager(client).createTable(EntryRepository.TABLE_NAME, [
+            {
+                name: "id",
+                property: "SERIAL PRIMARY KEY",
+            },
+            {
+                name: "value",
+                property: "TEXT",
+            },
+        ]);
+    }
+
+    static async reset(client: Client) {
+        await new TableManager(client).truncateTable(
+            EntryRepository.TABLE_NAME
+        );
+    }
+
+    async add(value: Entry["value"]): Promise<Entry["id"]> {
+        const id = await this.client.query(
+            `INSERT INTO ${EntryRepository.TABLE_NAME} (value) VALUES ($1) RETURNING id`,
+            [value]
+        );
+
+        return id.rows[0].id;
+    }
+
+    async getById(id: Entry["id"]): Promise<Entry> {
+        const result = await this.client.query(
+            `SELECT * FROM ${EntryRepository.TABLE_NAME} WHERE id = $1`,
+            [id]
+        );
+
+        if (!result) {
+            throw new Error(`Entry with id ${id} not found`);
+        }
+
+        return result.rows[0];
+    }
+
+    async count(): Promise<number> {
+        const result = await this.client.query(
+            `SELECT COUNT(*) FROM ${EntryRepository.TABLE_NAME}`
+        );
+
+        return parseInt(result.rows[0].count);
+    }
+}
+
+function repository(client: Client) {
+    return new EntryRepository(client);
+}
+
+function ignoringErrors(fn: () => Promise<void>) {
+    return async () => {
+        try {
+            await fn();
+        } catch {
+            // ignore
+        }
+    };
+}
 
 describe("PostgreSQL unit of work", () => {
     const dbManager = new DatabaseManager(replaceDatabaseName("postgres", URL));
-    const conn = new pg.Client(URL);
-    const tableManager = new TableManager(conn);
+    const connOnTheOutside = new Client(URL);
+    const orthagonalRepository = repository(connOnTheOutside);
     const uow = new PostgresUnitOfWork(
-        () => new pg.Client(URL),
-        (client) => client.end()
+        () => new Client(URL),
+        (conn) => conn.end()
     );
 
     beforeAll(async () => {
         await dbManager.createDatabase(DATABASE);
-        await tableManager.createTable(TABLE, [
-            {
-                name: "id",
-                property: "INT",
-            },
-        ]);
+
+        await connOnTheOutside.connect();
+        await EntryRepository.setup(connOnTheOutside);
 
         return async () => {
-            await tableManager.close();
+            await connOnTheOutside.end();
 
             await dbManager.dropDatabase(DATABASE);
             await dbManager.close();
@@ -42,199 +107,98 @@ describe("PostgreSQL unit of work", () => {
     });
 
     beforeEach(async () => {
-        await tableManager.truncateTable(TABLE);
+        await EntryRepository.reset(connOnTheOutside);
     });
 
-    test("getClient() outside of uow throws error", async () => {
-        expect(() => uow.getClient()).toThrowError(/.*not started.*/);
-    });
-
-    test("client passed as argument and client from .getClient() shares the same transaction", async () => {
-        await uow.wrap(async (client) => {
-            const txId = await getTxid(client);
-            await expect(getTxid(uow.getClient())).resolves.toBe(txId);
+    function doInNestedUow<T>(
+        fn: (runner: Client) => Promise<T>,
+        propagation: Propagation
+    ): Promise<T> {
+        return uow.wrap((runner) => fn(runner), {
+            propagation,
         });
-    });
+    }
 
-    test("committing", async () => {
-        await uow.wrap((c) => insert(c, 1));
+    async function addEntryInNestedUow(
+        value: string,
+        propagation: Propagation
+    ) {
+        return doInNestedUow(
+            (runner) => repository(runner).add(value),
+            propagation
+        );
+    }
 
-        expect(await exists(conn, 1)).toBe(true);
-    });
-
-    test("rolling back", async () => {
-        await expect(
-            uow.wrap(async (client) => {
-                await insert(client, 1);
-
-                throw new Error("rollback");
-            })
-        ).rejects.toThrowError("rollback");
-
-        expect(await exists(conn, 1)).toBe(false);
-    });
-
-    test("using existing transaction - same txid", async () => {
-        const txids = new Set();
-
-        await uow.wrap(async (client) => {
-            txids.add(await getTxid(client));
-
-            await uow.wrap(async (client) => txids.add(await getTxid(client)));
+    test("successful execution of wrapped function, results in committed state", async () => {
+        const entryId = await uow.wrap(async (runner) => {
+            return await repository(runner).add("test");
         });
 
-        expect(txids.size).toBe(1);
+        const entry = await orthagonalRepository.getById(entryId);
+        expect(entry.value).toBe("test");
     });
 
-    test("using existing transaction - rollback", async () => {
-        await uow.wrap(async (c1) => {
-            await insert(c1, 1);
+    test("transaction is rolled back when an error is thrown inside of fn", async () => {
+        const failingExecute = uow.wrap(async (runner) => {
+            await repository(runner).add("test");
 
-            try {
-                await uow.wrap(
-                    () => {
-                        throw new Error("rollback");
-                    },
-                    {
-                        propagation: Propagation.EXISTING,
-                    }
-                );
-            } catch {}
+            throw new Error("rollback");
         });
 
-        expect(await count(conn)).toBe(0);
+        await expect(failingExecute).rejects.toThrowError("rollback");
+
+        const noEntries = (await orthagonalRepository.count()) === 0;
+        expect(noEntries).toBe(true);
     });
 
-    test("using existing transaction - cannot use client after rollback", async () => {
-        await uow.wrap(async (c1) => {
-            try {
-                await uow.wrap(
-                    () => {
-                        throw new Error("rollback");
-                    },
-                    {
-                        propagation: Propagation.EXISTING,
-                    }
-                );
-            } catch {}
-
-            await expect(insert(c1, 1)).rejects.toThrowError(
-                UnitOfWorkAbortedError
-            );
-        });
-    });
-
-    test("using existing transaction - commit", async () => {
-        await uow.wrap(async (c1) => {
-            await insert(c1, 1);
-
-            await uow.wrap((c2) => insert(c2, 2));
-        });
-
-        expect(await count(conn)).toBe(2);
-    });
-
-    test("forcing new transaction", async () => {
-        const number = 5;
-        const txids = new Set();
-        function newUow() {
-            return uow.wrap(
-                async (client) => {
-                    txids.add(await getTxid(client));
-                },
-                { propagation: Propagation.NEW }
+    describe("rollback behavior", () => {
+        async function failingNestedUow(propagation: Propagation) {
+            return doInNestedUow(
+                ignoringErrors(() => {
+                    throw new Error("nested rollback");
+                }),
+                propagation
             );
         }
 
-        await uow.wrap(() => Promise.all(_.times(number, newUow)));
+        test("using propagation EXISTING: when child uow rolls back, the transaction is closed", async () => {
+            await ignoringErrors(() =>
+                uow.wrap(async () => {
+                    await failingNestedUow(Propagation.EXISTING);
 
-        expect(new Set(txids).size).toBe(number);
-    });
+                    const work = () =>
+                        addEntryInNestedUow("test", Propagation.EXISTING);
+                    await expect(work).rejects.toThrowError(
+                        UnitOfWorkAbortedError
+                    );
+                })
+            );
+        });
 
-    test("nested transactions", async () => {
-        const txids = new Set();
-        async function track(client: pg.Client, id: number) {
-            txids.add(await getTxid(client));
-            await insert(client, id);
-        }
+        test("using propagation NESTED: the transaction is alive even though child uow rolls back", async () => {
+            await uow.wrap(async () => {
+                await failingNestedUow(Propagation.NESTED);
 
-        await uow.wrap(async (c1) => {
-            await track(c1, 1);
-
-            try {
-                await uow.wrap(
-                    async (c2) => {
-                        await track(c2, 2);
-
-                        throw new Error("rollback");
-                    },
-                    { propagation: Propagation.NESTED }
+                const entryId = await addEntryInNestedUow(
+                    "test",
+                    Propagation.NESTED
                 );
-            } catch {}
-
-            await track(c1, 3);
-
-            await uow.wrap(async (c4) => track(c4, 4), {
-                propagation: Propagation.NESTED,
+                expect(entryId).toBeGreaterThan(0);
             });
         });
 
-        expect(txids.size).toBe(1);
+        test("using propagation NESTED: only the changes in the error-thrown uow are rolled back", async () => {
+            await uow.wrap(async () => {
+                await addEntryInNestedUow("1", Propagation.NESTED);
 
-        expect(await exists(conn, 1)).toBe(true);
-        expect(await exists(conn, 2)).toBe(false);
-        expect(await exists(conn, 3)).toBe(true);
-        expect(await exists(conn, 4)).toBe(true);
-    });
+                await failingNestedUow(Propagation.NESTED);
 
-    test.each([
-        IsolationLevel.READ_COMMITTED,
-        IsolationLevel.REPEATABLE_READ,
-        IsolationLevel.SERIALIZABLE,
-    ])("isolation level", async (isolationLevel) => {
-        await uow.wrap(
-            async (client) => {
-                const result = await client.query(
-                    "SHOW TRANSACTION ISOLATION LEVEL"
-                );
+                await addEntryInNestedUow("2", Propagation.NESTED);
+            });
 
-                expect(result.rows[0].transaction_isolation).toBe(
-                    isolationLevel
-                );
-            },
-            { isolationLevel }
-        );
-    });
-
-    test("anniehilating client", async () => {
-        let client!: pg.Client;
-
-        await uow.wrap(async () => {
-            client = uow.getClient();
+            const firstAndThirdAreCommitted =
+                (await orthagonalRepository.count()) === 2;
+            expect(firstAndThirdAreCommitted).toBe(true);
         });
-
-        await expect(getTxid(client)).rejects.toThrowError(/.*closed.*/);
     });
 });
-
-async function insert(client: pg.Client, id: number): Promise<void> {
-    await client.query(`INSERT INTO ${TABLE} VALUES ($1);`, [id]);
-}
-
-async function exists(client: pg.Client, id: number): Promise<boolean> {
-    const result = await client.query(
-        `SELECT COUNT(*) FROM ${TABLE} WHERE id = $1;`,
-        [id]
-    );
-    return result.rows[0].count > 0;
-}
-
-async function count(client: pg.Client): Promise<number> {
-    const result = await client.query(`SELECT COUNT(*) FROM ${TABLE};`);
-    return parseInt(result.rows[0].count);
-}
-
-async function getTxid(client: pg.Client): Promise<string> {
-    const result = await client.query(`SELECT txid_current();`);
-    return result.rows[0].txid_current;
-}
